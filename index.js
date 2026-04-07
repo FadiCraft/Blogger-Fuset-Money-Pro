dfe35bccfec886e993514fa9a4978101dbda8b3a
FadiCraft <crf0582@gmail.com>
2026-04-07 09:13:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -216,6 +216,7 @@ async function startEmpireBot() {
                         ${cleanAiBody}
                     </div>
 
+                    <a href="${data.link}" class="source-link" target="_blank" rel="nofollow">Read Full Research on Original Source ↗</a>
                 </div>
                 `;
 
