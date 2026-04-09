c44b4bfd691e34d0eb5130c9cd9b18dc6cfacf91
FadiCraft <crf0582@gmail.com>
2026-04-09 21:30:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const BLOG_ID = process.env.BLOG_ID || "2636919176960128451";
 const CLIENT_ID = process.env.CLIENT_ID || "872415365656-7qribadnc7k2u21kl6jjcbatdueevifh.apps.googleusercontent.com"; 
 const CLIENT_SECRET = process.env.CLIENT_SECRET || "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"; 
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN || "1//04yti9k2agPknCgYIARAAGAQSNwF-L9IrTZPKt5Fqbg2vrM9sBtOks9cnY4M7Idg0LToQnlbYGME06k20vcyr_SVmYk1H_yZJdEc"; 
-const GROQ_API_KEY = process.env.GROQ_API_KEY || "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"; 
+const GROQ_API_KEY = process.env.GROQ_API_KEY || "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"; 
 
 const groq = new Groq({ apiKey: GROQ_API_KEY });
 const parser = new Parser({
