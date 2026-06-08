c75c576c0136f7738f490727574cffb57b877d86
FadiCraft <crf0582@gmail.com>
2026-06-08 19:45:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path');
 const BLOG_ID = process.env.BLOG_ID || "2636919176960128451";
 const CLIENT_ID = process.env.CLIENT_ID || "872415365656-7qribadnc7k2u21kl6jjcbatdueevifh.apps.googleusercontent.com";
 const CLIENT_SECRET = process.env.CLIENT_SECRET || "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";
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN || "1//045S0zh6CVaa6CgYIARAAGAQSNwF-L9IrXvcqzbATZc7wlJFATwLq4Rh6IpFlGA2GIsnKEUjUV5h1lA9WCwNEq5tmP89y-wJIHKY";
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN || "1//046k2RWLveK4KCgYIARAAGAQSNwF-L9IrYJWfeeIkjStq18W7y0hun58uQ5ZaRwT3NP_feh7hE-LLRIg5RZ9-jDJqryVNN6fVhyU";
 
 const parser = new Parser({ timeout: 30000 });
 const HISTORY_FILE = path.join(__dirname, 'history.json');
